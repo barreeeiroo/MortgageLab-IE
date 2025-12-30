f38ac7d150eabf90e129b3ff5dc25be1c4c43145
Diego Barreiro Perez <diego@barreiro.dev>
2025-12-30 17:23:20 +0100

    Fix URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
+	site: "https://diego.barreiro.dev",
+	base: isProd ? "/MortgageLab-IE" : "/",
+
 	integrations: [react()],
 
 	vite: {
