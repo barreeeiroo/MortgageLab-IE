52d46b71a7cd3c406d2aa222f34f30e8e652d05e
Diego Barreiro Perez <diego@barreiro.dev>
2025-12-31 13:22:59 +0100

    Custom Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-const isProd = import.meta.env.PROD;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @returns {import('vite').Plugin} */
@@ -31,8 +30,8 @@ function serveDataFolder() {
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://diego.barreiro.dev",
-	base: isProd ? "/MortgageLab-IE" : "/",
+	site: "https://www.mortgagelab.ie",
+	base: "/",
 
 	integrations: [react()],
 
