582b88c4509ab40cfcb6f8c11ea274ad62d67279
Diego Barreiro Perez <diego@barreiro.dev>
2026-01-04 03:32:52 +0100

    Remove Barrel Exports

--- src/components/custom-rates/index.ts
+++
@@ -1,5 +0,0 @@
-export { AddCustomRateDialog } from "./AddCustomRateDialog";
-export { type CustomLenderInfo, CustomRateForm } from "./CustomRateForm";
-export { CustomRatesIsland } from "./CustomRatesIsland";
-export { EditCustomRateDialog } from "./EditCustomRateDialog";
-export { ManageCustomRatesDialog } from "./ManageCustomRatesDialog";
--- src/components/simulate/index.ts
+++
@@ -1,8 +0,0 @@
-// Re-export islands for external use
-export { SimulateChartIsland } from "./chart/SimulateChartIsland";
-export { SimulateEmptyState } from "./header/SimulateEmptyState";
-export { SimulateHeaderIsland } from "./header/SimulateHeaderIsland";
-export { SimulateMilestonesIsland } from "./sidebar/SimulateMilestonesIsland";
-export { SimulateOverpaymentsIsland } from "./sidebar/SimulateOverpaymentsIsland";
-export { SimulateRatesIsland } from "./sidebar/SimulateRatesIsland";
-export { SimulateTableIsland } from "./table/SimulateTableIsland";
