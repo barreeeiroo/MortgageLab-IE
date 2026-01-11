67e4c587300e31c123dc8874b76e50a940582bba
Diego Barreiro Perez <diego@barreiro.dev>
2026-01-11 16:22:01 +0000

    Optimize Lender Logos

--- src/components/lenders/LenderLogo.tsx
+++ src/components/lenders/LenderLogo.tsx
@@ -1,14 +1,14 @@
 import { Pencil } from "lucide-react";
 import aibLogo from "@/assets/logos/lenders/aib.webp";
-import avantLogo from "@/assets/logos/lenders/avant.png";
-import boiLogo from "@/assets/logos/lenders/boi.png";
-import cuLogo from "@/assets/logos/lenders/cu.png";
+import avantLogo from "@/assets/logos/lenders/avant.webp";
+import boiLogo from "@/assets/logos/lenders/boi.webp";
+import cuLogo from "@/assets/logos/lenders/cu.webp";
 import ebsLogo from "@/assets/logos/lenders/ebs.webp";
-import havenLogo from "@/assets/logos/lenders/haven.png";
-import icsLogo from "@/assets/logos/lenders/ics.png";
-import mocoLogo from "@/assets/logos/lenders/moco.png";
-import nuaLogo from "@/assets/logos/lenders/nua.png";
-import ptsbLogo from "@/assets/logos/lenders/ptsb.png";
+import havenLogo from "@/assets/logos/lenders/haven.webp";
+import icsLogo from "@/assets/logos/lenders/ics.webp";
+import mocoLogo from "@/assets/logos/lenders/moco.webp";
+import nuaLogo from "@/assets/logos/lenders/nua.webp";
+import ptsbLogo from "@/assets/logos/lenders/ptsb.webp";
 import {
 	Tooltip,
 	TooltipContent,
